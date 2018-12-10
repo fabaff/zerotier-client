cdb95a111f86ea7296a386cf0f059967f3f2adc7
Fabian Affolter <mail@fabian-affolter.ch>
2018-12-10 21:04:51 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Setup file for ZeroTier Python API client."""
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='zerotier_api',
+    version='0.1.0',
+    description='Python API for interacting with luftdaten.info.',
+    long_description=long_description,
+    url='https://github.com/fabaff/zerotier-client',
+    download_url='https://github.com/fabaff/zerotier-client/releases',
+    author='Fabian Affolter',
+    author_email='fabian@affolter-engineering.ch',
+    license='MIT',
+    install_requires=['aiohttp', 'async_timeout'],
+    packages=['zerotier_api'],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
+)
